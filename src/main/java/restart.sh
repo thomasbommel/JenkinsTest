7bba3aae9c9f7d832fc044afe89b571b1e9e58fa
bommel <thomasbommi@gmail.com>
2019-09-11 22:47:16 +0200

    fixed script

--- src/main/java/restart.sh
+++ src/main/java/restart.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "*********************Restarting Tomcat70.******************"
-sh /apps/apache/sss-tomcat70.ksh status
 echo "Trying to stop Tomcat."
-sh /apps/apache/sss-tomcat70.ksh stop
-echo "Getting Tomcat Status."
-sh /apps/apache/sss-tomcat70.ksh status
+service tomcat stop
+sleep 2
 echo "Trying to Start Tomcat"
-sh /apps/apache/sss-tomcat70.ksh start
+service tomcat start
 sleep 2
 echo "Getting Tomcat Status"
-sh /apps/apache/sss-tomcat70.ksh status
+service tomcat status
